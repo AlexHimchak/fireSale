01d7495d26ffb511cc8dfb0b70cbc1d3cfdef153
ais047 <hursungwoo@yahoo.com>
2017-08-03 15:35:49 -0700

    filestructure

---
+++ controllers/routes/api-routes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+var shop = require('../..models/shop.js');
+
+router.get("/", function(req,res){
+});
+
+
+router.post("/add", function(req,res){
+
+});
