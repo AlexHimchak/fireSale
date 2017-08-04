58d99ae6c0fabb47eff4eee97b3cc71919efc1a3
School <school@Davids-MacBook-Pro.local>
2017-08-03 20:39:58 -0700

    routes and models

---
+++ models/item.js
@@ -0,0 +1,37 @@
+module.exports = function(sequelize, DataTypes) {
+  var Item = sequelize.define("Item", {
+    itemTitle: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+      default: "http://via.placeholder.com/350x350"
+      allowNull: false
+    }
+    price: {
+      type: DataTypes.Decimal,
+      allowNull: false
+    }
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }
+  });
+
+  Item.associate = function(models) {
+    Post.belongsTo(models.Shop, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Post;
+};
+
+
--- models/shop.js
+++ models/shop.js
@@ -8,31 +8,12 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    item: {
-      type:DataTypes.STRING,
-      allowNull: false
-      image: {
-        type: DataTypes.STRING,
-        default: "http://via.placeholder.com/350x350"
-        allowNull: false
-      }
-      price: {
-        type: DataTypes.Decimal,
-        allowNull: false
-      }
-      stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      }
-    }
   });
 
-  //   Shop.associate = function(models) {
-  //   Shop.hasMany(models.Item, {
-  //     onDelete: "cascade"
-  //   });
-  // };
-
-
+  Shop.associate = function(models) {
+    Shop.hasMany(models.Item, {
+      onDelete: "cascade"
+    });
+  };
   return Shop;
 };
