76693016f19f08913b78eb7a5c8ea8211b1315c1
School <school@Davids-MacBook-Pro.local>
2017-08-03 19:35:40 -0700

    updated 'server.js' with route filenames

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 
 require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./routes/shop-api-routes.js")(app);
 //require("./routes/post-api-routes.js")(app);
 
 
